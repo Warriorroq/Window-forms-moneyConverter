4371db8873de448e5e0d75520541d3957598788a
Warriorroq <Warriorroq@gmail.com>
2021-04-11 15:57:03 +0300

    Corrected form (title, size)

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -1,17 +1,8 @@
-﻿
-namespace WinFormsApp1
+﻿namespace WinFormsApp1
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,13 +11,6 @@ namespace WinFormsApp1
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -34,8 +18,6 @@ namespace WinFormsApp1
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
         }
-
-        #endregion
     }
 }
 
